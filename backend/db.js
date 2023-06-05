48c85622f3a873973cd5372fc8a8f9c905b7afb1
pranjal sood <pranjalsood46@gmail.com>
2023-06-05 21:54:56 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://pranjalsood69:pranjal123@cluster0.s6segpm.mongodb.net/?retryWrites=true&w=majority";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
